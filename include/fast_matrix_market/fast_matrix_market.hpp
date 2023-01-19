4fb0d58c14b36f7987639fdf6f4dc45594e9dbea
Adam Lugowski <alugowski@gmail.com>
2023-01-19 15:28:15 -0800

    Version bump

--- include/fast_matrix_market/fast_matrix_market.hpp
+++ include/fast_matrix_market/fast_matrix_market.hpp
@@ -21,8 +21,8 @@ namespace fast_matrix_market {
     // Version macros.
     // Keep in sync with python/pyproject.toml
 #define FAST_MATRIX_MARKET_VERSION_MAJOR 1
-#define FAST_MATRIX_MARKET_VERSION_MINOR 3
-#define FAST_MATRIX_MARKET_VERSION_PATCH 1
+#define FAST_MATRIX_MARKET_VERSION_MINOR 4
+#define FAST_MATRIX_MARKET_VERSION_PATCH 0
 
     constexpr std::string_view kSpace = " ";
     constexpr std::string_view kNewline = "\n";
