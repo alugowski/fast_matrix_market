d819dbaae6854c75ea4f9807ac7adc4ce24fcd19
Adam Lugowski <alugowski@gmail.com>
2023-01-26 10:15:10 -0800

    Set c++17 standard for charconv tests
    MSVC doesn't have it enabled by default and the detection fails even though it should pass

--- cmake/to_chars_tests.cmake
+++ cmake/to_chars_tests.cmake
@@ -2,6 +2,11 @@
 # The floating point versions are specified in C++17, but compiler support varies.
 include(CheckSourceCompiles)
 
+# CMP0067: Honor language standard in try_compile() source-file signature.
+# https://cmake.org/cmake/help/latest/policy/CMP0067.html
+cmake_policy(SET CMP0067 NEW)
+set(CMAKE_CXX_STANDARD 17)
+
 # Check for int support
 check_source_compiles(CXX "
 #include <charconv>
