de932df525917f50c3ab092ab1d29091497e847a
Adam Lugowski <alugowski@gmail.com>
2023-05-09 16:15:57 -0700

    Improve example docs

--- examples/README.md
+++ examples/README.md
@@ -1,6 +1,14 @@
+Complete example of how to use `fast_matrix_market`.
+
+This directory is explicitly *not* part of the `fast_matrix_market` build system, because neither is your project.
+
+The [fast_matrix_market](fast_matrix_market) subdirectory simulates cloning the repo into a subdirectory of your project. "Simulates" because this directory just contains symlinks into the main repo so that the examples are always up-to-date. The symlinks also demonstrate the parts of the main repo that are necessary to use FMM.
+
+The example [CMakeLists.txt](CMakeLists.txt) uses `add_subdirectory()`, but of course any of the methods described in the main Installation section of the [README](../README.md) will work.
+
 ## Build and Run
 
-Build using the standard CMake commands:
+Build using standard CMake commands:
 
 ```bash
 cmake -S . -B cmake-build-debug/ -D CMAKE_BUILD_TYPE=Debug
@@ -9,6 +17,6 @@ cmake --build cmake-build-debug/
 cmake-build-debug/simple1
 ```
 
-### simple1
+### simple1.cpp
 
-Defines custom sparse and dense matrices, writes and reads both.
+Defines simple sparse and dense matrices built on `std::vector`, writes and reads both using `fast_matrix_market` triplet and array methods.
--- examples/simple1.cpp
+++ examples/simple1.cpp
@@ -6,6 +6,11 @@
 
 #include <fast_matrix_market/fast_matrix_market.hpp>
 
+// If you prefer more concise code then you can use this to abbreviate the
+// rather long namespace name. Then just use `fmm::` instead of `fast_matrix_market::`.
+// This is not used below only to make the code simpler to understand at a glance and for easy copy/paste.
+namespace fmm = fast_matrix_market;
+
 /**
  * A simple triplet sparse matrix.
  */
@@ -27,7 +32,7 @@ struct array_matrix {
 };
 
 int main() {
-    // create a matrix
+    // Create a matrix
     triplet_matrix<int64_t, double> triplet;
 
     triplet.nrows = 4;
@@ -39,28 +44,33 @@ int main() {
 
     std::string mm;
 
-    // write to a string
+    // Write triplet to Matrix Market. Use std::ostringstream to write to a string.
     {
         std::ostringstream oss;
 
+        // The `nrows` and `ncols` below are a brace initialization of the header.
+        // If you are interested in the other aspects of the Matrix Market header then
+        // construct an instance of fast_matrix_market::matrix_market_header.
+        // This is how you would manipulate the comment field: header.comment = std::string("comment");
+        // You may also set header.field = fast_matrix_market::pattern to write a pattern file (only indices, no values).
+        // Non-pattern field types (integer, real, complex) are deduced from the template type and cannot be overriden.
+
         fast_matrix_market::write_matrix_market_triplet(
                 oss,
-                fast_matrix_market::matrix_market_header(triplet.nrows, triplet.ncols),
+                {triplet.nrows, triplet.ncols},
                 triplet.rows, triplet.cols, triplet.vals);
 
         mm = oss.str();
         std::cout << mm << std::endl << std::endl;
     }
 
-    // read into another triplet
+    // Read Matrix Market into another triplet
     {
-        fast_matrix_market::matrix_market_header header;
-
         triplet_matrix<int64_t, double> triplet2;
         std::istringstream iss(mm);
-        fast_matrix_market::read_matrix_market_triplet(iss, header, triplet2.rows, triplet2.cols, triplet2.vals);
-        triplet2.nrows = header.nrows;
-        triplet2.ncols = header.ncols;
+        fast_matrix_market::read_matrix_market_triplet(iss,
+                                                       triplet2.nrows, triplet2.ncols,
+                                                       triplet2.rows, triplet2.cols, triplet2.vals);
 
         assert(triplet.nrows == triplet2.nrows &&
                triplet.ncols == triplet2.ncols &&
@@ -69,23 +79,32 @@ int main() {
                triplet.vals == triplet2.vals);
     }
 
-    // read into array
+    // Read Matrix Market into complex dense array
     array_matrix<std::complex<double>> array;
     {
-        fast_matrix_market::matrix_market_header header;
+        // Dense arrays can be row- or column-major. FMM defaults to row-major but can be used with either.
+        // Use the ordering that your code expects.
+        // Using the wrong one can lead to transposed values (for square matrices) or scrambled values.
+        //
+        // read_matrix_market_array() accepts any resizable vector type, such as std::vector, and will resize
+        // it to match the contents of the Matrix Market file.
+        //
+        // Note that the file being read happens to be a coordinate (sparse) file. That is ok, FMM performs the
+        // conversion automatically and efficiently. In this case the resulting dense array is allocated
+        // (and default initialized to zero by std::vector::resize()) then FMM writes only the values specified in the file.
         std::istringstream iss(mm);
-        fast_matrix_market::read_matrix_market_array(iss, header, array.vals);
-        array.nrows = header.nrows;
-        array.ncols = header.ncols;
+        fast_matrix_market::read_matrix_market_array(iss,
+                                                     array.nrows, array.ncols,
+                                                     array.vals, fast_matrix_market::row_major);
     }
 
-    // write array
+    // Write dense array to Matrix Market
     {
         std::ostringstream oss;
         fast_matrix_market::write_matrix_market_array(
                 oss,
-                fast_matrix_market::matrix_market_header(array.nrows, array.ncols),
-                array.vals);
+                {array.nrows, array.ncols},
+                array.vals, fast_matrix_market::row_major);
 
         mm = oss.str();
         std::cout << mm << std::endl << std::endl;
