7a6720d30e844e5fe210ca81ccdb83d1ab5ee562
Adam Lugowski <alugowski@gmail.com>
2023-09-08 19:42:01 -0700

    Support Python 3.12 wheels
    Upgrade to cibuildwheel 2.15

--- python/tests/test_basic.py
+++ python/tests/test_basic.py
@@ -22,6 +22,14 @@ class TestModule(unittest.TestCase):
     @unittest.skipIf(not threadpoolctl or not hasattr(threadpoolctl, "register"),
                      reason="no threadpoolctl or version too old")
     def test_threadpoolctl(self):
+        import os
+        import sys
+        import pytest
+        if sys.platform == "darwin" and sys.version_info.minor == 8 and os.environ.get("CIBUILDWHEEL", 0):
+            pytest.xfail("threadpoolctl fails inside cibuildwheel macOS Python 3.8")
+            # see https://github.com/joblib/threadpoolctl/issues/150
+            return
+
         with threadpoolctl.threadpool_limits(limits=2, user_api='fast_matrix_market'):
             self.assertEqual(fmm.PARALLELISM, 2)
         with threadpoolctl.threadpool_limits(limits=4):
